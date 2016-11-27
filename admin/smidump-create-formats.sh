9b92e869fc8e31900d8e40183fd58b1d21b1b203
Uwe Ebel <kobmaki@aol.com>
2016-11-27 12:34:51 +0100

    remote prefix KOBOLDMAKI-

--- admin/smidump-create-formats.sh
+++ admin/smidump-create-formats.sh
@@ -2,5 +2,5 @@
 
 # netsnmp scli
 for i in identifiers tree corba compliances smiv1  smiv2 netsnmp; do
-    smidump -f ${i} ../share/snmp/mibs/KOBOLDMAKI-ICINGA2-MIB.txt > ../htdocs/KOBOLDMAKI-ICINGA2-MIB.${i}.txt
+    smidump -f ${i} ../share/snmp/mibs/ICINGA2-MIB.txt > ../htdocs/ICINGA2-MIB.${i}.txt
 done
