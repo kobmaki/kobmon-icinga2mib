3bd10adf549e8194d081f9af3622755a8888a152
Uwe Ebel <kobmaki@aol.com>
2016-12-04 18:38:46 +0100

    use ICINGA-* as prefix instead of ICINGA2-*

--- admin/smidump-create-formats.sh
+++ admin/smidump-create-formats.sh
@@ -2,5 +2,5 @@
 
 # netsnmp scli
 for i in identifiers tree corba compliances smiv1  smiv2 netsnmp; do
-    smidump -f ${i} ../share/snmp/mibs/ICINGA2-MIB.txt > ../htdocs/ICINGA2-MIB.${i}.txt
+    smidump -f ${i} ../share/snmp/mibs/ICINGA-ICINGA2-MIB.txt > ../htdocs/ICINGA-ICINGA2-MIB.${i}.txt
 done
